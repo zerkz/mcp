91523e25a37b3b594ccc386b094bfa4b8c38ff2a
Mike Donnalley <mdonnalley@salesforce.com>
2025-06-16 09:17:44 -0600

    fix: set SF_LOG_LEVEL when using --debug

--- bin/run.js
+++ bin/run.js
@@ -4,6 +4,7 @@ if (process.argv.includes('--debug')) {
   process.env.DEBUG = 'sf*';
   process.env.SF_LOG_COLORIZE = 'false';
   process.env.SF_LOG_STDERR = 'true';
+  process.env.SF_LOG_LEVEL = 'trace';
 }
 
 import { execute } from '@oclif/core';
