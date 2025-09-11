c10355eaf376d55c535b4148bdd03db9243432bb
Willhoit <iowillhoit@users.noreply.github.com>
2025-09-11 17:08:19 -0500

    W-19618621: Pass a subset of startup flags to the Services class (#212)
    * feat: pass a subset of startup flags to the Services class
    
    * chore: fix tests

--- packages/mcp/src/services.ts
+++ packages/mcp/src/services.ts
@@ -20,6 +20,7 @@ import {
   OrgService,
   SanitizedOrgAuthorization,
   ConfigService,
+  StartupFlags
 } from '@salesforce/mcp-provider-api';
 import Cache from './utils/cache.js';
 import {
@@ -33,10 +34,16 @@ import {
 export class Services implements IServices {
   private readonly telemetry: TelemetryService;
   private readonly dataDir: string;
+  private readonly startupFlags: StartupFlags;
 
-  public constructor(opts: { telemetry: TelemetryService | undefined; dataDir: string }) {
+  public constructor(opts: {
+    telemetry: TelemetryService | undefined;
+    dataDir: string;
+    startupFlags: StartupFlags;
+  }) {
     this.telemetry = opts.telemetry ? opts.telemetry : new NoopTelemetryService();
     this.dataDir = opts.dataDir;
+    this.startupFlags = opts.startupFlags;
   }
 
   public getTelemetryService(): TelemetryService {
@@ -46,6 +53,7 @@ export class Services implements IServices {
   public getConfigService(): ConfigService {
     return {
       getDataDir: () => this.dataDir,
+      getStartupFlags: () => this.startupFlags
     };
   }
 
