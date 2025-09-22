fc976fc99382bf43e1628d76fdce838044bd8ff7
Joshua Feingold <jfeingold35@gmail.com>
2025-09-22 16:50:18 -0500

    @W-18964528@ explicitly importing vitest

--- packages/mcp/test/eval/experiment.eval.ts
+++ packages/mcp/test/eval/experiment.eval.ts
@@ -1,4 +1,7 @@
-
+// NOTE: Theoretically, we shouldn't need this, but having the mocha tests running alongside the
+//       vitest tests means that we run into problems where the test runner tries to use mocha instead.
+//       It's dumb, but here we are.
+import {describe, it, expect} from "vitest"
 
 describe('asdfasdfasdf', () => {
     it('math works', () => {
